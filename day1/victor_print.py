3608eed96014ba2625f19054bcd707453070c363
VictorNalu <victorchinalurumogu@gmail.com>
2025-11-13 20:23:01 +0100

    print using formatted string

---
+++ day1/victor_print.py
@@ -0,0 +1,13 @@
+#Using Formatted Print
+
+name = input("Enter your name: ")
+course = input("What's Your course? ")
+What_class = input("What's Your class? ")
+language = input("What Language Do You Speak? ")
+age = int(input("How Old Are You? "))
+
+#New line
+print()
+
+#Print statement using formatted string
+print(f"My name is {name}, and my course is {course}. I am in {What_class} and I speak {language}. I am {age} years old.")
